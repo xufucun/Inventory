1003443093ab798d415ea5567e7cb309e66cf04e
xufucun <xufucun@gmail.com>
2017-12-11 09:56:26 +0800

    抽空改一改

--- app/src/main/java/cn/xufucun/udacity/inventory/InfoActivity.java
+++ app/src/main/java/cn/xufucun/udacity/inventory/InfoActivity.java
@@ -1,7 +1,11 @@
 package cn.xufucun.udacity.inventory;
 
+import android.app.AlertDialog;
 import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
@@ -9,24 +13,42 @@ import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import cn.xufucun.udacity.inventory.data.InventoryContract;
 import cn.xufucun.udacity.inventory.databinding.ActivityInfoBinding;
 
-public class InfoActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+public class InfoActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>, View.OnClickListener {
 
     private ActivityInfoBinding infoBinding;
 
     private Uri mCurrentPetUri;
     private static final int EXISTING_PET_LOADER = 0;
 
+    private String contactTel;
+    private String contactEmail;
+
+
+    private int quantity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        infoBinding = DataBindingUtil.setContentView(this,R.layout.activity_info);
+        infoBinding = DataBindingUtil.setContentView(this, R.layout.activity_info);
 
         Intent intent = getIntent();
         mCurrentPetUri = intent.getData();
+
+        infoBinding.btnQuantityMinus.setOnClickListener(this);
+        infoBinding.btnQuantityEdit.setOnClickListener(this);
+        infoBinding.btnQuantityPlus.setOnClickListener(this);
+        infoBinding.btnSubscribeTel.setOnClickListener(this);
+        infoBinding.btnSubscribeEmail.setOnClickListener(this);
+        infoBinding.btnDeleteGoods.setOnClickListener(this);
 
         getLoaderManager().initLoader(EXISTING_PET_LOADER, null, this);
 
@@ -35,24 +57,15 @@ public class InfoActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        //需要的列
         String[] projection = {
                 InventoryContract.InventoryEntry._ID,
                 InventoryContract.InventoryEntry.COLUMN_INVENTORY_NAME,
                 InventoryContract.InventoryEntry.COLUMN_INVENTORY_QUANTITY,
-                InventoryContract.InventoryEntry.COLUMN_INVENTORY_PRICE,
-                InventoryContract.InventoryEntry.COLUMN_INVENTORY_BARCODE,
-                InventoryContract.InventoryEntry.COLUMN_SUPPLIER_NAME,
-                InventoryContract.InventoryEntry.COLUMN_SUPPLIER_CONTACT,
                 InventoryContract.InventoryEntry.COLUMN_SUPPLIER_PHONE_NUMBER,
-                InventoryContract.InventoryEntry.COLUMN_SUPPLIER_EMAIL };
+                InventoryContract.InventoryEntry.COLUMN_SUPPLIER_EMAIL};
 
-        // This loader will execute the ContentProvider's query method on a background thread
-        return new CursorLoader(this,   // Parent activity context
-                mCurrentPetUri,         // Query the content URI for the current pet
-                projection,             // Columns to include in the resulting Cursor
-                null,                   // No selection clause
-                null,                   // No selection arguments
-                null);                  // Default sort order
+        return new CursorLoader(this, mCurrentPetUri, projection, null, null, null);
     }
 
     @Override
@@ -65,31 +78,152 @@ public class InfoActivity extends AppCompatActivity implements LoaderManager.Loa
 
             int gNameColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_INVENTORY_NAME);
             int gQuantityColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_INVENTORY_QUANTITY);
-            int gPriceColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_INVENTORY_PRICE);
-            int gBarcodeColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_INVENTORY_BARCODE);
-            int sNameColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_SUPPLIER_NAME);
-            int sContactColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_SUPPLIER_CONTACT);
             int sPhoneNuberColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_SUPPLIER_PHONE_NUMBER);
             int sEmailColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_SUPPLIER_EMAIL);
 
             String gName = cursor.getString(gNameColumnIndex);
             int gQuantity = cursor.getInt(gQuantityColumnIndex);
-            int gPrice = cursor.getInt(gPriceColumnIndex);
-            String gBarcode = cursor.getString(gBarcodeColumnIndex);
-            String sName = cursor.getString(sNameColumnIndex);
-            String sContact = cursor.getString(sContactColumnIndex);
             String sPhoneNuber = cursor.getString(sPhoneNuberColumnIndex);
             String sEmail = cursor.getString(sEmailColumnIndex);
 
-
             infoBinding.tvGoodsName.setText(gName);
             infoBinding.tvGoodsQuantity.setText(String.valueOf(gQuantity));
+
+            contactTel = sPhoneNuber;
+            contactEmail = sEmail;
+
+            quantity = gQuantity;
 
         }
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
+        infoBinding.tvGoodsName.setText("");
+        infoBinding.tvGoodsQuantity.setText("");
 
+    }
+
+
+    private void showDeleteConfirmationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("确定要删除吗？");
+        builder.setMessage("删除将无法恢复,请谨慎使用！");
+        builder.setPositiveButton("删除", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                deleteGoods();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void deleteGoods() {
+        if (mCurrentPetUri != null) {
+            int rowsDeleted = getContentResolver().delete(mCurrentPetUri, null, null);
+            if (rowsDeleted == 0) {
+                Toast.makeText(this, "删除失败", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "删除成功", Toast.LENGTH_SHORT).show();
+            }
+        }
+        finish();
+    }
+
+    private void dialogEditQuantity(){
+        final EditText editText = new EditText(this);
+        AlertDialog.Builder inputDialog = new AlertDialog.Builder(this);
+        inputDialog.setTitle("输入要修改的数量").setView(editText);
+        inputDialog.setPositiveButton("确定",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+//                        Toast.makeText(MainActivity.this, editText.getText().toString(), Toast.LENGTH_SHORT).show();
+                        int q  = Integer.valueOf(editText.getText().toString());
+                        editQuantity(q);
+                    }
+                }).show();
+    }
+
+
+    /**
+     * 修改数量
+     * @param quantity 数量
+     */
+    private void editQuantity(int quantity){
+        ContentValues values = new ContentValues();
+        values.put(InventoryContract.InventoryEntry.COLUMN_INVENTORY_QUANTITY,quantity);
+        int rowsAffected = getContentResolver().update(mCurrentPetUri, values, null, null);
+        if (rowsAffected == 0) {
+            Toast.makeText(this, "修改失败", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "修改成功", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_info, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_edit:
+                Intent intent = new Intent(InfoActivity.this, EditorActivity.class);
+                intent.setData(mCurrentPetUri);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_quantity_minus:
+                if (quantity == 0) {
+                    return;
+                }
+                quantity = quantity -1;
+                editQuantity(quantity);
+                break;
+            case R.id.btn_quantity_plus:
+                quantity = quantity +1;
+                editQuantity(quantity);
+                break;
+            case R.id.btn_quantity_edit:
+                dialogEditQuantity();
+                break;
+            case R.id.btn_subscribe_tel:
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + contactTel));
+                startActivity(dialIntent);
+                break;
+            case R.id.btn_subscribe_email:
+                Intent i = new Intent(Intent.ACTION_SEND);
+                // i.setType("text/plain"); //模拟器请使用这行
+                i.setType("message/rfc822"); // 真机上使用这行
+                i.putExtra(Intent.EXTRA_EMAIL, new String[]{contactEmail});
+                i.putExtra(Intent.EXTRA_SUBJECT, "您的建议");
+                i.putExtra(Intent.EXTRA_TEXT, "我们很希望能得到您的建议！！！");
+                startActivity(Intent.createChooser(i, "Select email application."));
+                break;
+            case R.id.btn_delete_goods:
+                showDeleteConfirmationDialog();
+                break;
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/cn/xufucun/udacity/inventory/MainActivity.java
+++ app/src/main/java/cn/xufucun/udacity/inventory/MainActivity.java
@@ -12,19 +12,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
 import cn.xufucun.udacity.inventory.data.InventoryContract.InventoryEntry;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener,LoaderManager.LoaderCallbacks<Cursor> {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, LoaderManager.LoaderCallbacks<Cursor>, AdapterView.OnItemClickListener {
 
     private static final String TAG = "MainActivity";
     private static final int INVENTORY_LOADER = 0;
-    InventoryCursorAdapter mCursorAdapter;
+    private InventoryCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +34,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         ListView listView = findViewById(R.id.list_view);
-      //  View emptyView = View.inflate(this,R.layout.empty_view,null);
-      //  listView.setEmptyView(emptyView);
 
         mCursorAdapter = new InventoryCursorAdapter(this, null);
         listView.setAdapter(mCursorAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long id) {
-                Log.d(TAG, "onItemClick: 点击了列表项");
-                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
-                Uri currentPetUri = ContentUris.withAppendedId(InventoryEntry.CONTENT_URI, id);
-                intent.setData(currentPetUri);
-                startActivity(intent);
-            }
-        });
+        listView.setOnItemClickListener(this);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(this);
@@ -60,33 +51,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivity(intent);
     }
 
-
-//    @Override
-//    public void onItemClick(AdapterView<?> adapterView, View view, int i, long id) {
-//        Log.d(TAG, "onItemClick: 点击了列表项");
-//        Intent intent = new Intent(this, EditorActivity.class);
-//        Uri currentPetUri = ContentUris.withAppendedId(InventoryEntry.CONTENT_URI, id);
-//        intent.setData(currentPetUri);
-//        startActivity(intent);
-//    }
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long id) {
+        Log.d(TAG, "onItemClick: 点击了列表项");
+        Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+        Uri currentPetUri = ContentUris.withAppendedId(InventoryEntry.CONTENT_URI, id);
+        intent.setData(currentPetUri);
+        startActivity(intent);
+    }
 
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-
-        //定义一个投影来指定我们关心的表中的列。
+        //需要的列
         String[] projection = {
                 InventoryEntry._ID,
                 InventoryEntry.COLUMN_INVENTORY_NAME,
-                InventoryEntry.COLUMN_INVENTORY_PRICE,
-                InventoryEntry.COLUMN_INVENTORY_QUANTITY};
+                InventoryEntry.COLUMN_INVENTORY_QUANTITY,
+                InventoryEntry.COLUMN_INVENTORY_PRICE};
 
         //该加载器将在后台线程上执行ContentProvider的查询方法
-        return new CursorLoader(this,    // Parent activity context
-                InventoryEntry.CONTENT_URI,      // Provider content URI to query
-                projection,                      // Columns to include in the resulting Cursor
-                null,                   // No selection clause
-                null,               // No selection arguments
-                null);                 // Default sort order
+        return new CursorLoader(this, InventoryEntry.CONTENT_URI, projection, null, null, null);
     }
 
     @Override
@@ -97,42 +81,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
         mCursorAdapter.swapCursor(null);
-    }
-
-    //TODO 插入 删除
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            //TODO 选择菜单
-            default:
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    //插入虚拟数据
-    private void insertInventory(){
-        ContentValues values = new ContentValues();
-        values.put(InventoryEntry.COLUMN_INVENTORY_NAME, "商品名称");
-        values.put(InventoryEntry.COLUMN_INVENTORY_PRICE, "Terrier");
-//        values.put(InventoryEntry.COLUMN_PET_GENDER, InventoryEntry.GENDER_MALE);
-//        values.put(InventoryEntry.COLUMN_PET_WEIGHT, 7);
-
-        Uri newUri = getContentResolver().insert(InventoryEntry.CONTENT_URI, values);
-    }
-
-    //删除所有数据
-    private void deleteAllPets() {
-        int rowsDeleted = getContentResolver().delete(InventoryEntry.CONTENT_URI, null, null);
-        Log.v("CatalogActivity", rowsDeleted + " rows deleted from pet database");
     }
 
 }
